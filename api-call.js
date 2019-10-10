0defe79b412500812acf31ed0b0bbf1be3574ffc
joshua hahn <tailrin.jh@gmail.com>
2019-10-10 12:29:19 -0600

    added files

--- api-call.js
+++ api-call.js
@@ -1,27 +1,48 @@
 'use strict';
 
-function getDogImage() {
-  fetch(`https://dog.ceo/api/breeds/image/random/${$('#number-of-dogs').val()}`)
-    .then(response => response.json())
-    .then(responseJson => {
-      const arr =[];
-      responseJson.message.forEach(message => {
-        arr.push(`<li><img src="${message}" class="images" alt="picture of a dog"></li>`);
-        console.log(message);
-      });
-      $("#pictures").html(arr.join(""));
+const searchURL = 'https://api.github.com/users/';
+
+function generateListItem(repo){
+  return `<li><a href="${repo.html_url}"><h3>${repo.name}</h3><br>${repo.html_url}</a>`
+}
+
+
+function displayResults(responseJson) {
+  const arr = [];
+  responseJson.forEach(repo => {
+    arr.push(generateListItem(repo));
+  });
+  $('#results-list').html(arr.join(""))
+  $('#results').removeClass('hidden');
+}
+
+
+
+function getNews(query) {
+  const url = searchURL + `${query}/repos`;
+
+  console.log(url);
+
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-    
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    const searchTerm = $('#js-search-term').val();
+    getNews(searchTerm);
   });
 }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchForm();
-});
+$(watchForm);
